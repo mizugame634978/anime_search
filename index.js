ee38e106aaf254223b0f7064bee660e69d14048b
mizugame <ryouta1853@gamil.com>
2024-04-02 17:08:36 +0900

    選択したジャンルが表示できるように変更

--- index.js
+++ index.js
@@ -11,18 +11,48 @@ nowGenres = {};//これに選択したジャンルが入る
 }*/
 let displayGenres =(genre)=>{
   console.log("testです");  
-  console.log(genre.id);
+//   console.log(genre.id);
   //選択しているか確認
+
+
   //あん2
-  if(! nowGenres[genre.id]){
-    nowGenres[genre.id] = "checked";
-    console.log(nowGenres);
-  }else{
-    delete nowGenres[genre.id];
-  }
-  nowGenres.forEach(element => {
-    console.log(element);
-  });
+//        /*
+    if(! nowGenres[genre.id]){
+        //存在していないので追加
+        nowGenres[genre.id] = "checked";//obj関連
+        let inner_screen = document.getElementById("inner_screen");
+        let screen = document.getElementById("screen");
+        let div = document.createElement("div");
+        div.innerText = genre.id;
+        inner_screen.append(div);
+    }else{
+        delete nowGenres[genre.id];
+        let inner_screen = document.getElementById("inner_screen");
+        inner_screen.remove();
+        let screen = document.getElementById("screen");
+        let inner_screen_new = document.createElement("div");
+        inner_screen_new.setAttribute("id","inner_screen");
+        screen.append(inner_screen_new);
+        for(i in nowGenres){
+            let screen = document.getElementById("screen");
+            let tmp = document.getElementById("inner_screen");
+            let div = document.createElement("div");
+            div.innerText = i;
+            tmp.append(div);
+        }
+        
+
+    }
+
+    // for(i in nowGenres){
+    //     console.log(i);
+    //     let screen = document.getElementById("screen");
+    //     let div = document.createElement("div");
+    //     div.innerText = i;
+    //     document.body.append(div);
+    // }
+        //*/
+
 };
 /**
  * あん1：最初からジャンル名、チェックフラグがあるオブジェクトを用意しておいて、それをforで回して表示
